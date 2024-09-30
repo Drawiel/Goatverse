fbc50d920696efaacf8d7b4b0d7f4125156a6288
Drawiel <daur0704@outlook.com>
2024-09-30 09:41:14 -0600

    Cambios en ventana principal para adaptar a nuevo diseño, se agrega el movimiento de las tarjetas

---
+++ Goatverse/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme;
+
+namespace Goatverse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Card_MouseEnter(object sender, MouseEventArgs e) {
+            var card = sender as MaterialDesignThemes.Wpf.Card;
+
+            ThicknessAnimation marginAnimation = new ThicknessAnimation {
+                From = card.Margin,
+                To = new Thickness(20, 200, 0, 0),
+                Duration = TimeSpan.FromSeconds(0.3) 
+            };
+
+            
+            card.BeginAnimation(FrameworkElement.MarginProperty, marginAnimation);
+        }
+
+        private void Card_MouseLeave(object sender, MouseEventArgs e) {
+            var card = sender as MaterialDesignThemes.Wpf.Card;
+
+            ThicknessAnimation marginAnimation = new ThicknessAnimation {
+                From = card.Margin,
+                To = new Thickness(20, 570, 0, 0),
+                Duration = TimeSpan.FromSeconds(0.3)
+            };
+
+
+            card.BeginAnimation(FrameworkElement.MarginProperty, marginAnimation);
+        }
+    }
+}
