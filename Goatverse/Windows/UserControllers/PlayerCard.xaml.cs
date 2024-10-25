2b4d26efe451852a370a9c1a6acd37979ee4c9df
Isabella Méndez <isabella.hmdez@gmail.com>
2024-10-25 01:31:28 -0600

    UserControl de carta de lobby

---
+++ Goatverse/Windows/UserControllers/PlayerCard.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Goatverse.Windows.UserControllers {
+    /// <summary>
+    /// Lógica de interacción para PlayerCard.xaml
+    /// </summary>
+    public partial class PlayerCard : UserControl {
+        public PlayerCard() {
+            InitializeComponent();
+        }
+        
+        public static readonly DependencyProperty GamertagProperty = DependencyProperty.Register(
+            "Gamertag", typeof(string), typeof(PlayerCard), new PropertyMetadata("Gamertag"));
+
+        public string Gamertag {
+            get { return (string)GetValue(GamertagProperty); }
+            set { SetValue(GamertagProperty, value); }
+        }
+
+        public static readonly DependencyProperty LevelProperty = DependencyProperty.Register(
+            "Level", typeof(int), typeof(PlayerCard), new PropertyMetadata(0));
+
+        public int Level {
+            get { return (int)GetValue(LevelProperty); }
+            set { SetValue(LevelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register(
+            "ImageSource", typeof(ImageSource), typeof(PlayerCard), new PropertyMetadata(null));
+
+        public ImageSource ImageSource {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+    }
+}
